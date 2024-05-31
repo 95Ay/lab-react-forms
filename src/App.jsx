1e5d68d0378389ba866b061dca68d20f1ceb3637
Ayoub Fariji <ayoubfariji95@gmail.com>
2024-05-31 15:03:06 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -7,47 +7,95 @@ import StudentCard from "./components/StudentCard";
 import studentsData from "./assets/students.json";
 
 function App() {
-  const [students, setStudents] = useState(studentsData);
+  const [students, setStudents] = useState(studentsData);{
+  const [fullName, setFullName] = useState("");
+  const [image , setImage] = useState(""); 
+  const [phone , setPhone] = useState(""); 
+  const [email , setEmail] = useState("");
+  const [program , setProgram] = useState("");
+  const [graduationYear , setGraduationYear] = useState("2023");
+  const [graduated , setGraduated] = useState(false);
 
+  const handleFullNameInput = (e) => setFullName(e.target.value);
+
+  const handleImageInput = (e) => setImage(e.target.value);
+
+  const handlePhoneInput = (e) => setPhone(e.target.value);
+
+  const handleEmailInput = (e) => setEmail(e.target.value);
+
+  const handleProgramInput = (e) => setProgram(e.target.value);
+
+  const handleGraduationYearInput = (e) => setGraduationYear(e.target.value);
+
+  const handleGraduatedInput = (e) => setGraduated(e.target.checked);  
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newStudent = { fullName, image, phone, email, program, graduationYear, graduated };
+
+    setStudents([...students, newStudent]);
+
+    setFullName("");
+    setImage("");
+    setPhone("");
+    setEmail("");
+    setProgram("");
+    setGraduationYear(2023);
+    setGraduated(true);
+  }
 
   return (
     <div className="App pt-20">
       <Navbar />
 
       {/* FORM */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <span>Add a Student</span>
         <div>
           <label>
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input name="fullName" type="text" value= {fullName}
+            onChange={handleFullNameInput}
+            placeholder="Full Name" />
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input name="image" type="url" value= {image}
+              onChange={handleImageInput}
+            placeholder="Profile Image" />
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input name="phone" type="tel" value= {phone}
+             onChange={handlePhoneInput}
+            
+            placeholder="Phone" />
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input name="email" type="email" value= {email}
+                          onChange={handleEmailInput}
+            placeholder="Email" />
           </label>
         </div>
 
         <div>
           <label>
             Program
-            <select name="program">
+            <select name="program"
+             onChange={handleProgramInput}
+
+                          >
               <option value="">-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
               <option value="Data">Data</option>
-            </select>
+                          </select>
           </label>
 
           <label>
@@ -55,6 +103,8 @@ function App() {
             <input
               name="graduationYear"
               type="number"
+              value= {graduationYear}
+              onChange={handleGraduationYearInput}
               placeholder="Graduation Year"
               minLength={4}
               maxLength={4}
@@ -65,13 +115,15 @@ function App() {
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input name="graduated" type="checkbox" checked= {graduated}
+                          onChange={handleGraduatedInput} />
           </label>
 
           <button type="submit">Add Student</button>
         </div>
 
       </form>
+
       {/* FORM END */}
 
 
@@ -87,5 +139,5 @@ function App() {
     </div>
   );
 }
-
+}
 export default App;
